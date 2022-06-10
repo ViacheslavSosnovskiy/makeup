0d1469501152068b14a7bf91501da8cff4e992bb
Viacheslav <saintstill333@gmail.com>
2022-06-10 21:00:40 +0300

    add modal window

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,38 @@
         mobileMenu.classList.toggle('is-open');
     })
 })();
+
+function onCloseModal(e) {
+    refs.backdropElt.innerHTML = '';
+    refs.backdropElt.classList.remove('show-modal');
+    document.body.parentNode.style.overflow = 'visible'; //Анна: скролл работает при закрытой модалке
+    refs.bodyElt.classList.remove('fixed-body');
+    window.removeEventListener('keydown', onEscKeyDown);
+    if (
+      refs.libraryBtn.classList.contains('current') &&
+      refs.watchedBtnLibrary.classList.contains('activeBtn')
+    ) {
+      showWatchedList();
+    }
+    if (
+      refs.libraryBtn.classList.contains('current') &&
+      refs.queueBtnLibrary.classList.contains('activeBtn')
+    ) {
+      showQueueList();
+    }
+  }
+
+// закрытие модального окна по клику на бекдроп
+// function onBackdropClick(e) {
+//     if (e.currentTarget === e.target) {
+//       onCloseModal();
+//     }
+//   }
+  
+//   // закрытие модального окна при нажатии на escape
+//   function onEscKeyDown(e) {
+//     // console.log(e.key);
+//     if (e.code === 'Escape') {
+//       onCloseModal();
+//     }
+//   }
